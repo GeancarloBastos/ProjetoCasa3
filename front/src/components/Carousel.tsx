28f9db25d086fa7ebda7a16eebde0f85c69df5d0
GeancarloBastos <geancarloads@gmail.com>
2024-10-02 17:39:54 -0300

    Alteração no Carousel

--- front/src/components/Carousel.tsx
+++ front/src/components/Carousel.tsx
@@ -28,10 +28,8 @@ export default function Carousel() {
       >  
         {slides.map((slide, index) => (  
           <SwiperSlide key={index} className="relative flex justify-center items-center">  
-            <img src={slide} alt={`Slide ${index}`} className="w-full h-full object-cover" />  
-            {/* Adiciona a sobreposição sólida à esquerda */}  
-            <div className="absolute inset-y-0 left-0 w-1/3 bg-black opacity-70"></div>  
-            {/* Texto sobre a imagem */}  
+            <img src={slide} alt={`Slide ${index}`} className="w-full h-full object-cover" />
+            <div className="absolute inset-y-0 left-0 w-1/3 bg-black opacity-70"></div> 
             <div className="absolute left-0 top-0 p-16 text-white z-10 max-w-lg">  
               <h2 className="text-4xl font-bold mb-4">Design build kitchen the family</h2>  
               <p className="mb-4">Kitchens should be designed around what's truly important: family, food, and life.</p>  
