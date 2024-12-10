b0246e8486b2ab01ac429b673a3144b155b81c3f
Cauã Stocker Saraiva <caua91@outlook.com>
2024-12-10 16:54:14 -0300

    att cadastro

--- front/src/components/Cadastro.tsx
+++ front/src/components/Cadastro.tsx
@@ -9,7 +9,8 @@ type Inputs = {
   email: string;  
   senha: string;  
   confirmarSenha: string;  
-  aceitarTermos: boolean;  
+  aceitarTermos: boolean; 
+  telefone: string; 
 };  
 
 export default function Cadastro() {  
@@ -32,7 +33,8 @@ export default function Cadastro() {
         body: JSON.stringify({  
           nome: data.nome,  
           email: data.email,  
-          senha: data.senha  
+          senha: data.senha,
+          telefone: data.telefone  
         }),  
       }  
     );  
@@ -45,89 +47,99 @@ export default function Cadastro() {
     }  
   }  
 
-  return (  
-    <div className="max-w-md mx-auto mb-32 p-6">  
-      <h2 className="mt-32 mb-6 text-3xl font-bold text-center">  
-        Criar Conta  
-      </h2>  
-      
-      <form onSubmit={handleSubmit(realizarCadastro)}>  
-        <div className="mb-5">  
-          <label className="block mb-2 text-sm font-medium text-gray-900">  
-            Nome Completo:  
-          </label>  
-          <input  
-            type="text"  
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"  
-            required  
-            {...register("nome")}  
-          />  
-        </div>  
+  return (
+    <div className="max-w-md mx-auto mb-32 p-6">
+      <h2 className="mt-32 mb-6 text-3xl font-bold text-center">Criar Conta</h2>
 
-        <div className="mb-5">  
-          <label className="block mb-2 text-sm font-medium text-gray-900">  
-            E-mail:  
-          </label>  
-          <input  
-            type="email"  
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"  
-            required  
-            {...register("email")}  
-          />  
-        </div>  
+      <form onSubmit={handleSubmit(realizarCadastro)}>
+        <div className="mb-5">
+          <label className="block mb-2 text-sm font-medium text-gray-900">
+            Nome Completo:
+          </label>
+          <input
+            type="text"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            required
+            {...register("nome")}
+          />
+        </div>
 
-        <div className="mb-5">  
-          <label className="block mb-2 text-sm font-medium text-gray-900">  
-            Senha:  
-          </label>  
-          <input  
-            type="password"  
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"  
-            required  
-            {...register("senha")}  
-          />  
-        </div>  
+        <div className="mb-5">
+          <label className="block mb-2 text-sm font-medium text-gray-900">
+            E-mail:
+          </label>
+          <input
+            type="email"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            required
+            {...register("email")}
+          />
+        </div>
 
-        <div className="mb-5">  
-          <label className="block mb-2 text-sm font-medium text-gray-900">  
-            Confirmar Senha:  
-          </label>  
-          <input  
-            type="password"  
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"  
-            required  
-            {...register("confirmarSenha")}  
-          />  
-        </div>  
+        <div className="mb-5">
+          <label className="block mb-2 text-sm font-medium text-gray-900">
+            Telefone/Celular:
+          </label>
+          <input
+            type="text"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            required
+            {...register("telefone")}
+          />
+        </div>
 
-        <div className="flex items-start mb-5">  
-          <div className="flex items-center h-5">  
-            <input  
-              type="checkbox"  
-              className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300"  
-              required  
-              {...register("aceitarTermos")}  
-            />  
-          </div>  
-          <label className="ms-2 text-sm font-medium text-gray-900">  
-            Li e aceito os termos de uso e política de privacidade  
-          </label>  
-        </div>  
+        <div className="mb-5">
+          <label className="block mb-2 text-sm font-medium text-gray-900">
+            Senha:
+          </label>
+          <input
+            type="password"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            required
+            {...register("senha")}
+          />
+        </div>
 
-        <button  
-          type="submit"  
-          className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-4"  
-        >  
-          Criar Conta  
-        </button>  
+        <div className="mb-5">
+          <label className="block mb-2 text-sm font-medium text-gray-900">
+            Confirmar Senha:
+          </label>
+          <input
+            type="password"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            required
+            {...register("confirmarSenha")}
+          />
+        </div>
 
-        <p className="text-center text-sm text-gray-600">  
-          Já possui uma conta?{" "}  
-          <Link href="/login" className="text-blue-600 hover:underline">  
-            Fazer Login  
-          </Link>  
-        </p>  
-      </form>  
-    </div>  
+        <div className="flex items-start mb-5">
+          <div className="flex items-center h-5">
+            <input
+              type="checkbox"
+              className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300"
+              required
+              {...register("aceitarTermos")}
+            />
+          </div>
+          <label className="ms-2 text-sm font-medium text-gray-900">
+            Li e aceito os termos de uso e política de privacidade
+          </label>
+        </div>
+
+        <button
+          type="submit"
+          className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-4"
+        >
+          Criar Conta
+        </button>
+
+        <p className="text-center text-sm text-gray-600">
+          Já possui uma conta?{" "}
+          <Link href="/login" className="text-blue-600 hover:underline">
+            Fazer Login
+          </Link>
+        </p>
+      </form>
+    </div>
   );  
 }
